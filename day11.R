0dfc4ccff3acd08b87f92c82d6ddcff52f137d1c
salientsoph <salientsoph@gmail.com>
2020-09-24 20:08:15 +0900

    R

--- day11.R
+++ day11.R
@@ -269,7 +269,7 @@ legend(8, 3, c("국어","수학"), cex=1.2, col=c("blue","red"), pch=c(21,16), l
 
 
 
-(성적 <- read.table("data/성적.txt", header=TRUE))
+(성적 <- read.table("c:/Rexam/data/성적.txt", header=TRUE))
 
 plot(성적$학번, 성적$국어, main="성적그래프", xlab="학번", ylab="점수",  xlim=c(0, 11), ylim=c(0, 11)) 
 
@@ -294,102 +294,3 @@ plot(국어, 수학)
 plot(수학~국어)
 
 ?plot
-
-# 막대그래프 그리기
-
-barplot(국어)
-
-coldens <- seq(from=10, to=100, by=10)   # 막대그래프의 색밀도 설정을 위한 벡터
-xname <- 성적$학번                                         # X 축 값 설정위한  벡터
-barplot(성적$국어, main="성적그래프", xlab="학번", ylab="점수", border="red", col="green", density=coldens, names.arg=xname, family="dog")
-
-# 학생의 각 과목에 대한 각각의 점수에 대한 그래프
-성적1<- 성적[3:5] 
-str(성적1)
-par(mar=c(5,5,5,5), mfrow=c(1,1))
-barplot(as.matrix(성적1), main="성적그래프", beside=T, ylab="점수", col=rainbow(10), family="cat")
-
-par(mar=c(5,5,5,5), mfrow=c(1,2))
-barplot(as.matrix(성적1), main="성적그래프", ylab="점수", col=rainbow(10))
-barplot(t(성적1), main="성적그래프", ylab="점수", col=rainbow(10))
-
-par(mar=c(5,5,5,5), mfrow=c(1,1))
-# 학생의 각 과목에 대한 합계 점수에 대한 그래프
-xname <- 성적$학번;    #  x축 레이블용 벡터
-par(xpd=T, mar=par()$mar+c(0,0,0,4));   # 우측에 범례가 들어갈 여백을 확보
-barplot(t(성적1), main="성적그래프", ylab="점수", col=rainbow(3), space=0.1, cex.axis=0.8, names.arg=xname, cex=0.8)
-legend(11,30, names(성적1), cex=0.8, fill=rainbow(3));
-
-par(xpd=T, mar=c(5,5,5,5));   # 우측에 범례가 들어갈 여백을 확보
-barplot(t(성적1), main="성적그래프", ylab="점수", col=rainbow(3), space=0.1, cex.axis=0.8, names.arg=xname, cex=0.8)
-legend(11,30, names(성적1), cex=0.8, fill=rainbow(3));
-
-
-# 학생의 각 과목에 대한 합계 점수에 대한 그래프(가로막대 그래프)
-xname <- 성적$학번;    #  x축 레이블용 벡터
-barplot(t(성적1), main="성적그래프", ylab="학번", col=rainbow(3), space=0.1, cex.axis=2.0, names.arg=xname,cex.lab=3.0, horiz=T);
-legend(30,11, names(성적1), cex=0.8, fill=rainbow(3))
-
-?barplot
-
-
-# 파이그래프
-(성적 <- read.table("data/성적.txt", header=TRUE));
-pie(성적$국어, labels=paste(성적$성명, "-", 성적$국어), col=rainbow(10))
-pie(성적$국어, clockwise=T, labels=paste(성적$성명, "-", 성적$국어), col=rainbow(10))
-pie(성적$국어, density=10, clockwise=T, labels=paste(성적$성명, "-", 성적$국어), col=rainbow(10))
-pie(성적$국어, labels=paste(성적$성명, "-", 성적$국어), col=rainbow(10), main="국어성적", edges=10)
-pie(성적$국어, labels=paste(성적$성명,"\n","(",성적$국어,")"), col=rainbow(10))
-pie(rep(1, 24), col = rainbow(24), radius = 0.5)
-
-
-# 히스토그램
-hist(성적$국어, main="성적분포-국어", 
-       xlab="점수", breaks=5, 
-       col = "lightblue", border = "pink", family='dog')
-hist(성적$수학, main="성적분포-수학", 
-       xlab="점수", col = "lightblue", 
-       breaks=2, border = "pink")
-hist(성적$국어, main="성적분포-국어", 
-       xlab="점수", ylab="도수", 
-       col=rainbow(12), border = "pink")
-
-nums <- sample(1:100, 30)
-hist(nums, family='maple')
-hist(nums, breaks=c(0,10,20,30,40,50,60,70,80,90,100))
-hist(nums, breaks=c(0,50,100), probability = T)
-hist(nums, breaks=c(0,33,66,100))
-
-
-# 박스플롯
-summary(성적$국어)
-boxplot(성적$국어, col="yellow",  ylim=c(0,10), xlab="국어", ylab="성적", family='dog')
-
-
-성적2 <- 성적[,3:5]
-boxplot(성적2, col=rainbow(3), ylim=c(0,10), ylab="성적")
-
-data <- read.table("data/온도.txt", header=TRUE, sep=",")
-head(data, n=5) 
-boxplot(data)
-boxplot(data, las = 2)
-boxplot(data, las = 2, at = c(1,2,3,4, 6,7,8,9, 11,12,13,14))
-chtcols = rep(c("red","sienna","palevioletred1","royalblue2"), times=3)
-chtcols
-boxplot(data, las = 2, at = c(1,2,3,4, 6,7,8,9, 11,12,13,14), col=chtcols)
-grid(col="gray", lty=2, lwd=1)
-
-boxplot(data, family='maple')  # 안됨!! 다른 아이들은 다 되는데!!
-
-boxplot(data, axes=F)
-axis(1, at=1:12, lab=names(data), family="maple") # x축 추가
-axis(2, at=seq(-5, 15, 5), family="dog")
-title("다양하게 폰트를 지정한 박스플롯", family="cat", cex.main=2)
-box()
-
-rainbow()
-heat.colors()
-terrain.colors()
-topo.colors()
-cm.colors()
-gray.colors()
